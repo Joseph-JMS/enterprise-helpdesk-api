efd17166b0261f38ed412f2efef5b69f215ff63d
Joseph Montoya <j.jhoel@outlook.es>
2026-02-02 16:20:47 -0500

    first commit

---
+++ src/main/java/com/devgroup/enterprise_helpdesk_api/EnterpriseHelpdeskApiApplication.java
@@ -0,0 +1,13 @@
+package com.devgroup.enterprise_helpdesk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseHelpdeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseHelpdeskApiApplication.class, args);
+	}
+
+}
